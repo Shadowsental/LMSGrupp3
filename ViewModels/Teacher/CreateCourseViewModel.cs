e427d6e75d61ced3639634f55196a383e70c1182
amigo500 <alexhouser@hotmail.com>
2022-03-24 20:17:24 +0100

    commit create course as a teacher etc.

---
+++ ViewModels/Teacher/CreateCourseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMSGrupp3.ViewModels.Teacher
+{
+    public class CreateCourseViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [Display(Name = "Start Date")]
+        [DataType(DataType.Date)]
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+        public DateTime StartDate { get; set; }
+    }
+}
